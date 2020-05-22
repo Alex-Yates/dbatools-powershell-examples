07a68f8d19b7f9f9891cbd65e8e0782c884c6fb4
Alex Yates <alex.yates@dlmconsultants.com>
2020-05-22 10:25:06 +0100

    Adding better logging to the sql script

--- sql_script.sql
+++ sql_script.sql
@@ -1,6 +1,13 @@
 USE MyDatabase
-if not exists (select * from sys.tables where name='hello_world')
-    create table hello_world (
-        col varchar(64) not null
-    )
+IF not exists (select * from sys.tables where name='hello_world')
+    BEGIN
+        PRINT 'Creating hello_world table'
+        CREATE TABLE hello_world (
+            col varchar(64) not null
+        )
+    END
+ELSE
+    BEGIN
+        PRINT 'hello_world table already exists, skipping update'
+    END
 go

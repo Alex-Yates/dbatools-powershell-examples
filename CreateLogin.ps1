5c2f53b2de1575b6999bbd9fd2231feae8aa379e
Alex Yates <alex.yates@dlmconsultants.com>
2020-05-22 09:52:16 +0100

    Adding the code for creating logins

--- CreateLogin.ps1
+++ CreateLogin.ps1
@@ -1,2 +1,5 @@
-$securePassword = Read-Host "Enter a secure password:" -AsSecureString
-New-DbaLogin -SqlInstance localhost -Login MyLogin -SecurePassword $securePassword -PasswordPolicyEnforced -PasswordExpirationEnabled
+# To run in a non-interactive mode, such as through an Octopus deployment, you will most likely need to pass the new login credentials as a PSCredential object.
+$securePassword = ConvertTo-SecureString "Th!sI5Y0urP4ss" -AsPlainText -Force
+
+# Create the login using the New-DbaLogin cmdlet
+New-DbaLogin -SqlInstance localhost -Login MyLogin2 -SecurePassword $securePassword -PasswordPolicyEnforced -PasswordExpirationEnabled
